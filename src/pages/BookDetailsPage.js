e9264af435fc840123111c6b6472b6afa050d55a
Viktor N <viktor.nestorov@web.de>
2023-07-05 02:46:55 +0200

    Changed page

--- src/pages/BookDetailsPage.js
+++ src/pages/BookDetailsPage.js
@@ -1,60 +1,55 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
-import AddBook from "../components/AddBook";
-import BookCard from "../components/BookCard";
+import AddEvent from "../components/AddEvent";
 
-function EventDetailsPage(props) {
-  const [event, setEvent] = useState(null);
-  const { eventId } = useParams();
+function BookDetailsPage(props) {
+  const [book, setBook] = useState(null);
+  const { bookId } = useParams();
 
-  const getEvent = () => {
+  const getBook = () => {
     const storedToken = localStorage.getItem("authToken");
 
     axios
-      .get(`${process.env.REACT_APP_SERVER_URL}/api/events/${eventId}`, {
+      .get(`${process.env.REACT_APP_SERVER_URL}/api/books/${bookId}`, {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then((response) => {
-        const oneEvent = response.data;
-        setEvent(oneEvent);
+        const oneBook = response.data;
+        setBook(oneBook);
       })
       .catch((error) => console.log(error));
   };
 
   useEffect(() => {
-    getEvent();
+    getBook();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="container">
-      {event && (
+      {book && (
         <>
-          <h1>{event.title}</h1>
-          <p>{event.location}</p>
-          <p>{event.address}</p>
-          <p>{event.description}</p>
-          <p>{event.time}</p>
-          <p>{event.date}</p>
-          <p>{event.comments}</p>
+          <h1>{book.title}</h1>
+          <p>Author: {book.author}</p>
+          <p>Description: {book.description}</p>
+          <p>Genre: {book.genre}</p>
+          <p>Availability: {book.availability}</p>
+          <p>Comments: {book.comments}</p>
         </>
       )}
 
-      <Link to="/events">
-        <button className="btn btn-primary">Back to Events</button>
+      <Link to="/books">
+        <button className="btn btn-primary">Back to Books</button>
       </Link>
 
-      <Link to={`/events/edit/${eventId}`}>
-        <button className="btn btn-primary">Edit Event</button>
+      <Link to={`/books/edit/${bookId}`}>
+        <button className="btn btn-primary">Edit Book</button>
       </Link>
 
-      <AddBook refreshEvent={getEvent} eventId={eventId} />
-
-      {event &&
-        event.books.map((book) => <BookCard key={book._id} {...book} />)}
+      <AddEvent refreshBook={getBook} bookId={bookId} />
     </div>
   );
 }
 
-export default EventDetailsPage;
+export default BookDetailsPage;
